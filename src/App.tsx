ac40fe2964db78932f68c709a4f3dcc5e063ac5c
Yuri Brasil <yurialvesbrasil@gmail.com>
2021-06-22 23:23:47 -0300

    Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from './components/Button';
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+import { BrowserRouter, Route } from 'react-router-dom';
+import {AuthContextProvider} from './context/AuthContext';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button />
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
